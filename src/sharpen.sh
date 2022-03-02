f30d9035fb2f9aa004aaf3d896803f8c6e93f627
ki11errabbit <unlikelytitan@gmail.com>
2022-03-01 17:53:19 -0700

    Initiall commit

---
+++ src/sharpen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+$PWD/Black/x1/sharpen &
+$PWD/Black/x1_25/sharpen &
+$PWD/Black/x1_5/sharpen &
+$PWD/Black/x2/sharpen &
+
+$PWD/White/x1/sharpen &
+$PWD/White/x1_25/sharpen &
+$PWD/White/x1_5/sharpen &
+$PWD/White/x2/sharpen &
